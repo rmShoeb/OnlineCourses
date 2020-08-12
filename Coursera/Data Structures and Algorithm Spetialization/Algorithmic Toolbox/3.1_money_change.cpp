34be058d0a7e1e51b567a2e45e99c032aeb6d966
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2020-08-12 19:21:17 +0600

    Create 3.1_money_change.cpp

---
+++ Coursera/Data Structures and Algorithm Spetialization/Algorithmic Toolbox/3.1_money_change.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int get_change(int m) {
+  int n = 0;
+  n += (m/10);
+  m %= 10;
+
+  n += (m/5);
+  m %= 5;
+
+  n += m;
+
+  return n;
+}
+
+int main() {
+  int m;
+  std::cin >> m;
+  std::cout << get_change(m) << '\n';
+}
