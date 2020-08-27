89760aaa71eeaf4f614ab05ef7abf898b478da32
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2020-08-27 23:23:17 +0600

    Create 04-using_subplots(2).py

---
+++ Datacamp/IntroductionToDataVisualizationInPython/01-CustomizingPlots/04-using_subplots(2).py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+# Create a figure with 2x2 subplot layout and make the top left subplot active
+plt.subplot(2,2,1)
+
+# Plot in blue the % of degrees awarded to women in the Physical Sciences
+plt.plot(year, physical_sciences, color='blue')
+plt.title('Physical Sciences')
+
+# Make the top right subplot active in the current 2x2 subplot grid 
+plt.subplot(2,2,2)
+
+# Plot in red the % of degrees awarded to women in Computer Science
+plt.plot(year, computer_science, color='red')
+plt.title('Computer Science')
+
+# Make the bottom left subplot active in the current 2x2 subplot grid
+plt.subplot(2,2,3)
+
+# Plot in green the % of degrees awarded to women in Health Professions
+plt.plot(year, health, color='green')
+plt.title('Health Professions')
+
+# Make the bottom right subplot active in the current 2x2 subplot grid
+plt.subplot(2,2,4)
+
+# Plot in yellow the % of degrees awarded to women in Education
+plt.plot(year, education, color='yellow')
+plt.title('Education')
+
+# Improve the spacing between subplots and display them
+plt.tight_layout()
+plt.show()
