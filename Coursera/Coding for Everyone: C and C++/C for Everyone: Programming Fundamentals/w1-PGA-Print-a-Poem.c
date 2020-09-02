d92e8177da0083290e7a333d01d33f7408b4a1d0
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2020-09-02 14:22:06 +0600

    Create w1-PGA-Print-a-Poem.c

---
+++ Coursera/Coding for Everyone: C and C++/C for Everyone: Programming Fundamentals/w1-PGA-Print-a-Poem.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(void)
+{   
+    printf("If you see my dad in Heaven\nHe won't be hard to find.\nHe'll be the one to greet you first,\nFor he's a one-of-a-kind.\n");
+    return 0;
+} 
