8637bfb21fc1ccce574a61835faac1f2b8b4385b
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2020-09-09 19:19:16 +0600

    Create m2-generating-meshes.py

---
+++ Datacamp/IntroductionToDataVisualizationInPython/01-CustomizingPlots/m2-generating-meshes.py
@@ -0,0 +1,20 @@
+# Import numpy and matplotlib.pyplot
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Generate two 1-D arrays: u, v
+u = np.linspace(-2, 2, 41)
+v = np.linspace(-1, 1, 21)
+
+# Generate 2-D arrays from u and v: X, Y
+X,Y = np.meshgrid(u, v)
+
+# Compute Z based on X and Y
+Z = np.sin(3*np.sqrt(X**2 + Y**2))
+
+# Display the resulting image with pcolor()
+plt.pcolor(Z)
+plt.show()
+
+# Save the figure to 'sine_mesh.png'
+plt.savefig('sine_mesh.png')
