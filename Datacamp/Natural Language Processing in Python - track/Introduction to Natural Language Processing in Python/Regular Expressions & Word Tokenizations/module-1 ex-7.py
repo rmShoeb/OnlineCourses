d470c73694d21e801d3710e782eb2ec36a8ade8a
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-01 01:29:46 +0600

    Create module-1 ex-7.py

---
+++ Datacamp/Natural Language Processing in Python - track/Introduction to Natural Language Processing in Python/Regular Expressions & Word Tokenizations/module-1 ex-7.py
@@ -0,0 +1,11 @@
+# Tokenize and print all words in german_text
+all_words = word_tokenize(german_text)
+print(all_words)
+
+# Tokenize and print only capital words
+capital_words = r"[A-ZÜ]\w+"
+print(regexp_tokenize(german_text, capital_words))
+
+# Tokenize and print only emoji
+emoji = "['\U0001F300-\U0001F5FF'|'\U0001F600-\U0001F64F'|'\U0001F680-\U0001F6FF'|'\u2600-\u26FF\u2700-\u27BF']"
+print(regexp_tokenize(german_text, emoji))
