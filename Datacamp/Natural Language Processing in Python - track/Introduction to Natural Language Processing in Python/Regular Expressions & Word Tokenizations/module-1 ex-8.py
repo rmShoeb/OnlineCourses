6c16c2f07cd62cee21d3a065729313c843941614
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-01 01:38:57 +0600

    Create module-1 ex-8.py

---
+++ Datacamp/Natural Language Processing in Python - track/Introduction to Natural Language Processing in Python/Regular Expressions & Word Tokenizations/module-1 ex-8.py
@@ -0,0 +1,18 @@
+# Split the script into lines: lines
+lines = holy_grail.split('\n')
+
+# Replace all script lines for speaker
+pattern = "[A-Z]{2,}(\s)?(#\d)?([A-Z]{2,})?:"
+lines = [re.sub(pattern, '', l) for l in lines]
+
+# Tokenize each line: tokenized_lines
+tokenized_lines = [regexp_tokenize(s, r"\w+") for s in lines]
+
+# Make a frequency list of lengths: line_num_words
+line_num_words = [len(t_line) for t_line in tokenized_lines]
+
+# Plot a histogram of the line lengths
+plt.hist(line_num_words)
+
+# Show the plot
+plt.show()
