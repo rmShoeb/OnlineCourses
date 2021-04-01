8f718c0744eba392f04eb6ea053dd54e5988b4d9
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-01 19:51:53 +0600

    Create module-3 ex-1.py

---
+++ Datacamp/Natural Language Processing in Python - track/Introduction to Natural Language Processing in Python/module-3 ex-1.py
@@ -0,0 +1,17 @@
+# Tokenize the article into sentences: sentences
+sentences = sent_tokenize(article)
+
+# Tokenize each sentence into words: token_sentences
+token_sentences = [word_tokenize(sent) for sent in sentences]
+
+# Tag each tokenized sentence into parts of speech: pos_sentences
+pos_sentences = [nltk.pos_tag(sent) for sent in token_sentences]
+
+# Create the named entity chunks: chunked_sentences
+chunked_sentences = nltk.ne_chunk_sents(pos_sentences, binary=True)
+
+# Test for stems of the tree with 'NE' tags
+for sent in chunked_sentences:
+    for chunk in sent:
+        if hasattr(chunk, "label") and chunk.label() == "NE":
+            print(chunk)
