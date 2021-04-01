6fae6fed52fd3cf09bc57213e2c406f2935e56de
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-01 20:10:54 +0600

    Create module-3 ex-4.py

---
+++ Datacamp/Natural Language Processing in Python - track/Introduction to Natural Language Processing in Python/module-3 ex-4.py
@@ -0,0 +1,12 @@
+# Import spacy
+import spacy
+
+# Instantiate the English model: nlp
+nlp = spacy.load('en', tagger=False, parser=False, matcher=False)
+
+# Create a new document: doc
+doc = nlp(article)
+
+# Print all of the found entities and their labels
+for ent in doc.ents:
+    print(ent.label_, ent.text)
