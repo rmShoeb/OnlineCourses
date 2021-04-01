03b6378198b0f4a48b7cda0422e33fb8742db9ae
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-02 00:26:22 +0600

    Create module-4 ex-3.py

---
+++ Datacamp/Natural Language Processing in Python - track/Introduction to Natural Language Processing in Python/module-4 ex-3.py
@@ -0,0 +1,24 @@
+# Import the necessary modules
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
+# Print the head of df
+print(df.head())
+
+# Create a series to store the labels: y
+y = df.label
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(df['text'], y, test_size=0.33, random_state=53)
+
+# Initialize a CountVectorizer object: count_vectorizer
+count_vectorizer = CountVectorizer(stop_words='english')
+
+# Transform the training data using only the 'text' column values: count_train 
+count_train = count_vectorizer.fit_transform(X_train.values)
+
+# Transform the test data using only the 'text' column values: count_test 
+count_test = count_vectorizer.transform(X_test.values)
+
+# Print the first 10 features of the count_vectorizer
+print(count_vectorizer.get_feature_names()[:10])
