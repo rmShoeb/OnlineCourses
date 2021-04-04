909e59c4e8eded1e9db3607c6f0dae6a4ab1a559
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-04 22:11:25 +0600

    Create module-3 ex-4.py

---
+++ Datacamp/Natural Language Processing in Python - track/Setiment Analysis in Python/module-3 ex-4.py
@@ -0,0 +1,12 @@
+# part-2
+# Build the first vectorizer
+vect1 = CountVectorizer().fit(tweets.text)
+vect1.transform(tweets.text)
+
+# Build the second vectorizer
+vect2 = CountVectorizer(token_pattern=r'\b[^\d\W][^\d\W]').fit(tweets.text)
+vect2.transform(tweets.text)
+
+# Print out the length of each vectorizer
+print('Length of vectorizer 1: ', len(vect1.get_feature_names()))
+print('Length of vectorizer 2: ', len(vect2.get_feature_names()))
