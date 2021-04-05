51a7779083ea956401b5a8741fc53dc0a69a7661
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-05 21:04:47 +0600

    Create chapter-2 ex-4.py

---
+++ Datacamp/Natural Language Processing in Python - track/Building Chatbots in Python/chapter-2 ex-4.py
@@ -0,0 +1,18 @@
+# Load the spacy model: nlp
+nlp = spacy.load('en')
+
+# Calculate the length of sentences
+n_sentences = len(sentences)
+
+# Calculate the dimensionality of nlp
+embedding_dim = nlp.vocab.vectors_length
+
+# Initialize the array with zeros: X
+X = np.zeros((n_sentences, embedding_dim))
+
+# Iterate over the sentences
+for idx, sentence in enumerate(sentences):
+    # Pass each each sentence to the nlp object to create a document
+    doc = nlp(sentence)
+    # Save the document's .vector attribute to the corresponding row in X
+    X[idx, :] = doc.vector
