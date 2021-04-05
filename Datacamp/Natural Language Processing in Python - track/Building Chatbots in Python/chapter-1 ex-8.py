9f955f531642b42b28224e4ff192afc120c51443
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-05 19:35:54 +0600

    Create chapter-1 ex-8.py

---
+++ Datacamp/Natural Language Processing in Python - track/Building Chatbots in Python/chapter-1 ex-8.py
@@ -0,0 +1,16 @@
+# Define respond()
+def respond(message):
+    # Call match_rule
+    response, phrase = match_rule(rules, message)
+    if '{0}' in response:
+        # Replace the pronouns in the phrase
+        phrase = replace_pronouns(phrase)
+        # Include the phrase in the response
+        response = response.format(phrase)
+    return response
+
+# Send the messages
+send_message("do you remember your last birthday")
+send_message("do you think humans should be worried about AI")
+send_message("I want a robot friend")
+send_message("what if you could be anything you wanted")
