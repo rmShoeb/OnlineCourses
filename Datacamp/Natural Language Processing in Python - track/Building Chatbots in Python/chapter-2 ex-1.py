b5c0fe6d57866a76ab8bf7fcb381aede39479df3
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-05 20:04:46 +0600

    Create chapter-2 ex-1.py

---
+++ Datacamp/Natural Language Processing in Python - track/Building Chatbots in Python/chapter-2 ex-1.py
@@ -0,0 +1,10 @@
+# Define a dictionary of patterns
+patterns = {}
+
+# Iterate over the keywords dictionary
+for intent, keys in keywords.items():
+    # Create regular expressions and compile them into pattern objects
+    patterns[intent] = re.compile('|'.join(keys))
+    
+# Print the patterns
+print(patterns)
