1443b60d2096b9f490f634e17a9a2b9b080c2114
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-04-30 15:43:31 +0600

    Update chapter-1 ex-7.py

--- Datacamp/Deep Learning for NLP in Python/Recurrent Neural Networks for Language Modeling in Python/chapter-1 ex-7.py
+++ Datacamp/Deep Learning for NLP in Python/Recurrent Neural Networks for Language Modeling in Python/chapter-1 ex-7.py
@@ -1,3 +1,5 @@
+# part-1
+####################################################################
 # Instantiate the class
 model = Sequential(name="sequential_model")
 
@@ -10,4 +12,21 @@ model.add(Dense(1, activation="sigmoid", name="output"))
 
 # The summary shows the layers and the number of parameters 
 # that will be trained
+model.summary()
+####################################################################
+# part-2
+####################################################################
+# Define the input layer
+main_input = Input(shape=(None, 10), name="input")
+
+# One LSTM layer (input shape is already defined)
+lstm_layer = LSTM(128, name="LSTM")(main_input)
+
+# Add a dense layer with one unit
+main_output = Dense(1, activation="sigmoid", name="output")(lstm_layer)
+
+# Instantiate the class at the end
+model = Model(inputs=main_input, outputs=main_output, name="modelclass_model")
+
+# Same amount of parameters to train as before (71,297)
 model.summary()
