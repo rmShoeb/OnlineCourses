85cd572e6622a6f2a4084d09fb88bfba4da00e08
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-05-02 13:48:18 +0600

    Create chapter-4 ex-1.py

---
+++ Datacamp/Deep Learning for NLP in Python/Recurrent Neural Networks for Language Modeling in Python/chapter-4 ex-1.py
@@ -0,0 +1,14 @@
+# Context for Sheldon phrase
+sheldon_context = "I’m not insane, my mother had me tested. "
+
+# Generate one Sheldon phrase
+sheldon_phrase = generate_sheldon_phrase(sheldon_model, sheldon_context)
+
+# Print the phrase
+print(sheldon_phrase)
+
+# Context for poem
+poem_context = "May thy beauty forever remain"
+
+# Print the poem
+print(generate_poem(poem_model, poem_context))
