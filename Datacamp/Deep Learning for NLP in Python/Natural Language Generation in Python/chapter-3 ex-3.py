9b101e24c069d44ac3e26b7516c869cd97ccd332
Riyad Morshed Shoeb <33808943+rmShoeb@users.noreply.github.com>
2021-05-28 21:58:39 +0600

    Create chapter-3 ex-3.py

---
+++ Datacamp/Deep Learning for NLP in Python/Natural Language Generation in Python/chapter-3 ex-3.py
@@ -0,0 +1,13 @@
+# part-1
+# Dictionary to contain the character to integer mapping for English
+eng_char_to_idx = dict((char, idx) for idx, char in enumerate(english_vocab))
+
+# Dictionary to contain the integer to character mapping for English
+eng_idx_to_char = dict((idx, char) for idx, char in enumerate(english_vocab))
+
+# part-2
+# Dictionary to contain the character to integer mapping for French
+fra_char_to_idx = dict((char, idx) for idx, char in enumerate(french_vocab))
+
+# Dictionary to contain the integer to character mapping for French
+fra_idx_to_char = dict((idx, char) for idx, char in enumerate(french_vocab))
